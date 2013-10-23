1a37d75760379da44a1516e5611f8e5bdbbd3596
Rich Wareham <rjw57@cam.ac.uk>
2013-10-23 17:33:30 +0100

    add initial script from Matt

---
+++ matches.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+
+start_time = time.clock()
+
+MIN_MATCH_COUNT = 10
+
+img1 = cv2.imread('front.bmp',0) # queryImage
+img2 = cv2.imread('rear.bmp',0)  # trainImage
+
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+matchesMask = mask.ravel().tolist()
+
+h,w = img1.shape
+pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+dst = cv2.perspectiveTransform(pts,M)
+
+
+print time.clock() - start_time, "seconds"
